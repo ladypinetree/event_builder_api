f556699558e4cea45af1a95b9b965847947fbd38
ladypinetree <maureenpine@gmail.com>
2017-03-28 18:44:57 -0400

    Add in create form to events index
    Next Steps:
    - Route for edit button
    - Component and route for delete button
    - Search?

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,6 @@ class Event < ApplicationRecord
     belongs_to :user
     # has_many :event_items
     # validates :event_items, presence: true
-    validates :user, presence: true
     validates :event_title, presence: true
     validates :event_type, presence: true
     validates :event_date, presence: true
